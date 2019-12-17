OxfordIonTrapGroup/stabilizer

commit 14dc750778213ff73ea0ec4b9e40c5ef609e5f0b

Merged stabilizer_adc_log and feedforward branches. Included function to set v_offset.

src/board.rs
@@ -409,6 +409,37 @@ fn spi4_setup(spi4: &pac::SPI4) {
     spi4.cr1.modify(|_, w| w.cstart().started());
 }
 
+// GPIO header SPI
+fn spi3_setup(spi3: &pac::SPI3) {
+    // AD5541
+    // max 25 MHz
+    // 16 bit word
+    spi3.cfg1.modify(|_, w|
+        w.mbr().div8()
+         .dsize().bits(16 - 1)
+         .fthlv().one_frame()
+    );
+    spi3.cfg2.modify(|_, w|
+        w.afcntr().controlled()
+         .ssom().not_asserted()
+         .ssoe().enabled()
+         .ssiop().active_low()
+         .ssm().disabled()
+         .cpol().idle_low()
+         .cpha().first_edge()
+         .lsbfrst().msbfirst()
+         .master().master()
+         .sp().motorola()
+         .comm().transmitter()
+         .ioswp().disabled()
+         .midi().bits(0)
+         .mssi().bits(0)
+    );
+    spi3.cr2.modify(|_, w| w.tsize().bits(0));
+    spi3.cr1.write(|w| w.spe().enabled());
+    spi3.cr1.modify(|_, w| w.cstart().started());
+}
+
 fn tim2_setup(tim2: &pac::TIM2) {
     tim2.psc.write(|w| w.psc().bits(200 - 1));  // from 200 MHz
     tim2.arr.write(|w| unsafe { w.bits(2 - 1) });  // µs
@@ -467,6 +498,19 @@ fn dma1_setup(dma1: &pac::DMA1, dmamux1: &pac::DMAMUX1, ma: usize, pa0: usize, p
     dma1.st[1].cr.modify(|_, w| w.en().set_bit());
 }
 
+fn cpu_dac_setup(cpu_dac: &pac::DAC){
+    // (optional) Disable CPU-DAC internal output buffer
+    // cpu_dac.mcr.modify(|_, w| unsafe{
+    //                   w.mode1().bits(0b010)
+    //                    .mode2().bits(0b010)
+    //                    });
+    // enable CPU-DAC outputs 1 and 2
+    cpu_dac.cr.modify(|_, w|
+                      w.en1().set_bit()
+                       .en2().set_bit()
+                       );
+}
+
 fn tim1_setup(tim1: &pac::TIM1) {
     tim1.arr.write(|w| unsafe { w.bits(feedforward::TMR_ARR_NOMINAL) });
     tim1.dier.write(|w| w.uie().set_bit()); // Interrupt on overflow
@@ -487,7 +531,7 @@ fn tim1_setup(tim1: &pac::TIM1) {
 static mut DAT: u32 = 0x201;  // EN | CSTART
 
 pub fn init() {
-    let mut cp = unsafe{ cortex_m::Peripherals::steal() }; 
+    let mut cp = unsafe{ cortex_m::Peripherals::steal() };
     let dp = unsafe{ pac::Peripherals::steal()};
 
     let rcc = dp.RCC;
@@ -530,6 +574,15 @@ pub fn init() {
     let spi5 = dp.SPI5;
     spi5_setup(&spi5);
     // spi5.ier.write(|w| w.eotie().set_bit());
+
+    // moveing before DMA configuration fixed broken SPI interrupt loop
+    rcc.apb1lenr.modify(|_, w| w.spi3en().set_bit());
+    let spi3 = dp.SPI3;
+    spi3_setup(&spi3);
+
+    // configure cpu_dac
+    let cpu_dacx = dp.DAC;
+    cpu_dac_setup(&cpu_dacx);
 
     rcc.ahb2enr.modify(|_, w|
         w

set_feedforward.py
@@ -1,17 +1,29 @@
 import json
 import socket
 from collections import OrderedDict as OD
+import argparse
 
-ip = "192.168.1.100"
+ip = "10.255.6.56"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((ip, 1237))
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("-s", "--stabilizer", default="10.255.6.56")
+    p.add_argument("-S", "--sin_amps", nargs='+', default=[0.]*5, type=float,
+                   help="Sin amplitudes as [50Hz 100Hz 150Hz 200Hz 250Hz] in units of full scale")
+    p.add_argument("-C", "--cos_amps", nargs='+', default=[0.]*5, type=float,
+                   help="Cos amplitudes as [50Hz 100Hz 150Hz 200Hz 250Hz] in units of full scale")
+    p.add_argument("-o", "--offset", default=0., type=float,
+                   help="Feed forward offset")
+    args = p.parse_args()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((ip, 1237))
 
-    sin_amplitudes = [0.5, 0., 0., 0., 0.]
-    cos_amplitudes = [0., 0., 0.1, 0., 0.]
+        sin_amplitudes = args.sin_amps
+        cos_amplitudes = args.cos_amps
+        offset = args.offset
 
-    msg = OD([("sin_amplitudes", sin_amplitudes), ("cos_amplitudes", cos_amplitudes)])
-    raw_msg = json.dumps(msg, separators=(",", ":"))
-    s.sendall(raw_msg.encode() + b"\n")
-    data = s.recv(1024)
-    print(data.decode())
+        msg = OD([("sin_amplitudes", sin_amplitudes), ("cos_amplitudes", cos_amplitudes), ("offset", offset)])
+        raw_msg = json.dumps(msg, separators=(",", ":"))
+        s.sendall(raw_msg.encode() + b"\n")
+        data = s.recv(1024)
+        print(data.decode())

src/feedforward.rs
@@ -20,6 +20,7 @@ pub struct State {
 pub struct Settings {
     pub sin_amplitudes: [f32; N_HARMONICS],
     pub cos_amplitudes: [f32; N_HARMONICS],
+    pub offset: f32
 }
 
 
@@ -33,8 +34,8 @@ impl Waveform {
         Waveform{ amplitude: [0; N_LOOKUP] }
     }
 
-    // Calculates the feedforward signal. This is called whenever the Fourier 
-    // coefficients are changed, so that in the interrupt loop we do the minimal 
+    // Calculates the feedforward signal. This is called whenever the Fourier
+    // coefficients are changed, so that in the interrupt loop we do the minimal
     // amount of work
     pub fn update(&mut self, settings: &Settings)
     {
@@ -52,7 +53,8 @@ impl Waveform {
             let harmonic_phase = phase*((i+1) as f32);
             sum += s.sin_amplitudes[i] * libm::sinf(harmonic_phase);
             sum += s.cos_amplitudes[i] * libm::cosf(harmonic_phase);
+            sum += s.offset * 0.2_f32;
         }
-        return (0x7fff as f32 * sum) as i16;  
+        return (0x7fff as f32 * sum) as i16;
     }
 }

commit 0928606222b37c5f3ae33656e1ff819f695b0443

Fixup breakage from 14dc750 merge [DM]

src/board.rs
@@ -137,7 +137,7 @@ fn io_compensation_setup(syscfg: &pac::SYSCFG) {
     while syscfg.cccsr.read().ready().bit_is_clear() {}
 }
 
-fn gpio_setup(gpioa: &pac::GPIOA, gpiob: &pac::GPIOB, gpiod: &pac::GPIOD,
+fn gpio_setup(gpioa: &pac::GPIOA, gpiob: &pac::GPIOB, gpioc: &pac::GPIOC, gpiod: &pac::GPIOD,
               gpioe: &pac::GPIOE, gpiof: &pac::GPIOF, gpiog: &pac::GPIOG) {
     // FP_LED0
     gpiod.otyper.modify(|_, w| w.ot5().push_pull());
@@ -297,6 +297,25 @@ fn gpio_setup(gpioa: &pac::GPIOA, gpiob: &pac::GPIOB, gpiod: &pac::GPIOD,
     // Enable PA8 as AF input
     gpioa.moder.modify(|_, w| w.moder8().alternate()); // moder0 corresponds to pin 0 on GPIOA
     gpioa.afrh.modify(|_, w| w.afr8().af1()); // AF1 = TIM1_CH1
+
+    // SPI3 - current sense board
+    // SCK: PC10
+    gpioc.moder.modify(|_, w| w.moder10().alternate());
+    gpioc.otyper.modify(|_, w| w.ot10().push_pull());
+    gpioc.ospeedr.modify(|_, w| w.ospeedr10().very_high_speed());
+    gpioc.afrh.modify(|_, w| w.afr10().af6());
+    // MOSI: PC12
+    gpioc.moder.modify(|_, w| w.moder12().alternate());
+    gpioc.otyper.modify(|_, w| w.ot12().push_pull());
+    gpioc.ospeedr.modify(|_, w| w.ospeedr12().very_high_speed());
+    gpioc.afrh.modify(|_, w| w.afr12().af6());
+    // MISO: PB4
+    // NSS: PA15
+    gpioa.moder.modify(|_, w| w.moder15().alternate());
+    gpioa.otyper.modify(|_, w| w.ot15().push_pull());
+    gpioa.ospeedr.modify(|_, w| w.ospeedr15().very_high_speed());
+    gpioa.afrh.modify(|_, w| w.afr15().af6());
+
 }
 
 // ADC0
@@ -526,6 +545,45 @@ fn tim1_setup(tim1: &pac::TIM1) {
         w.cen().set_bit());  // enable
 }
 
+fn usart3_setup(usart3: &pac::USART3)
+{
+    let baudrate = 1_000_000;
+    let sysclk = 100_000_000;
+
+    // Calculate baudrate divisor
+    let usartdiv = sysclk / baudrate;
+
+    // 16 times oversampling, OVER8 = 0
+    let brr = usartdiv as u16;
+    usart3.brr.write(|w| { w.brr().bits(brr) });
+
+    // Reset registers to disable advanced USART features
+    usart3.cr2.reset();
+    usart3.cr3.reset();
+
+    // Enable transmission and receiving
+    // and configure frame
+    usart3.cr1.write(|w| {
+        w.fifoen().set_bit() // FIFO mode enabled
+        .over8().oversampling16() // Oversampling by 16
+        .ue().enabled()
+        .te().enabled()
+        .re().enabled()
+    });
+}
+
+fn usart3_write(byte: u8) {
+    let usart3 = unsafe{&*pac::USART3::ptr()};
+    loop {
+        let isr = usart3.isr.read();
+
+        if isr.txe().bit_is_set() {
+            break;
+        }
+    }
+    usart3.tdr.write(|w| unsafe { w.bits(byte as u32)});
+}
+
 
 #[link_section = ".sram1.datspi"]
 static mut DAT: u32 = 0x201;  // EN | CSTART
@@ -541,6 +599,13 @@ pub fn init() {
     rcc.apb4enr.modify(|_, w| w.syscfgen().set_bit());
     io_compensation_setup(&dp.SYSCFG);
 
+    // Set up USART debugger
+    rcc.apb1lenr.modify(|_, w| w.usart3en().set_bit());
+    rcc.apb1lrstr.modify(|_, w| w.usart3rst().set_bit());
+    rcc.apb1lrstr.modify(|_, w| w.usart3rst().clear_bit());
+    let usart3 = dp.USART3;
+    usart3_setup(&usart3);
+
     cp.SCB.enable_icache();
     // TODO: ETH DMA coherence issues
     // cp.SCB.enable_dcache(&mut cp.CPUID);
@@ -555,7 +620,7 @@ pub fn init() {
         .gpiofen().set_bit()
         .gpiogen().set_bit()
     );
-    gpio_setup(&dp.GPIOA, &dp.GPIOB, &dp.GPIOD, &dp.GPIOE, &dp.GPIOF, &dp.GPIOG);
+    gpio_setup(&dp.GPIOA, &dp.GPIOB, &dp.GPIOC, &dp.GPIOD, &dp.GPIOE, &dp.GPIOF, &dp.GPIOG);
 
     rcc.apb1lenr.modify(|_, w| w.spi2en().set_bit());
     let spi2 = dp.SPI2;
@@ -579,6 +644,11 @@ pub fn init() {
     rcc.apb1lenr.modify(|_, w| w.spi3en().set_bit());
     let spi3 = dp.SPI3;
     spi3_setup(&spi3);
+
+    // enable cpu_dac clock
+    rcc.apb1lenr.modify(|_, w| w.dac12en().set_bit());
+    // manual suggests wait for domain to leave standby/sleep
+    while rcc.cr.read().d2ckrdy().is_not_ready() {}
 
     // configure cpu_dac
     let cpu_dacx = dp.DAC;

src/main.rs
@@ -118,67 +118,7 @@ impl log::Log for SerialLog {
     fn flush(&self) {}
 }
 
-// Pull in build information (from `built` crate)
-mod build_info {
-    #![allow(dead_code)]
-    // include!(concat!(env!("OUT_DIR"), "/built.rs"));
-}
-
-fn usart3_setup(usart3: &pac::USART3)
-{
-    let baudrate = 1_000_000;
-    let sysclk = 100_000_000;
-
-    // Calculate baudrate divisor
-    let usartdiv = sysclk / baudrate;
-
-    // 16 times oversampling, OVER8 = 0
-    let brr = usartdiv as u16;
-    usart3.brr.write(|w| { w.brr().bits(brr) });
-
-    // Reset registers to disable advanced USART features
-    usart3.cr2.reset();
-    usart3.cr3.reset();
-
-    // Enable transmission and receiving
-    // and configure frame
-    usart3.cr1.write(|w| {
-        w.fifoen().set_bit() // FIFO mode enabled
-        .over8().oversampling16() // Oversampling by 16
-        .ue().enabled()
-        .te().enabled()
-        .re().enabled()
-    });
-}
-
-fn usart3_write(byte: u8) {
-    let usart3 = unsafe{&*pac::USART3::ptr()};
-    loop {
-        let isr = usart3.isr.read();
-
-        if isr.txe().bit_is_set() {
-            break;
-        }
-    }
-    usart3.tdr.write(|w| unsafe { w.bits(byte as u32)});
-}
-
-
-fn cpu_dac_setup(cpu_dac: &pac::DAC){
-    // (optional) Disable CPU-DAC internal output buffer
-    // cpu_dac.mcr.modify(|_, w| unsafe{
-    //                   w.mode1().bits(0b010)
-    //                    .mode2().bits(0b010)
-    //                    });
-    // enable CPU-DAC outputs 1 and 2
-    cpu_dac.cr.modify(|_, w|
-                      w.en1().set_bit()
-                       .en2().set_bit()
-                       );
-}
-
 const SCALE: f32 = ((1 << 15) - 1) as f32;
-
 #[link_section = ".sram1.datspi"]
 static mut DAT: u32 = 0x201;  // EN | CSTART
 
@@ -237,25 +177,14 @@ const APP: () = {
     #[init(schedule = [tick])]
     fn init(c: init::Context) -> init::LateResources {
         board::init();
-
         let dp = unsafe { pac::Peripherals::steal() };
-        let rcc = dp.RCC;
-        rcc.apb1lenr.modify(|_, w| w.usart3en().set_bit());
-        rcc.apb1lrstr.modify(|_, w| w.usart3rst().set_bit());
-        rcc.apb1lrstr.modify(|_, w| w.usart3rst().clear_bit());
-        let usart3 = dp.USART3;
-        usart3_setup(&usart3);
+        let ff_waveform = feedforward::Waveform::new();
 
         init_log();
         // info!("Version {} {}", build_info::PKG_VERSION, build_info::GIT_VERSION.unwrap());
         // info!("Built on {}", build_info::BUILT_TIME_UTC);
         // info!("{} {}", build_info::RUSTC_VERSION, build_info::TARGET);
 
-        // c.schedule.tick(Instant::now()).unwrap();
-
-        let ff_waveform = feedforward::Waveform::new();
-
-        // c.schedule.tick(Instant::now()).unwrap();
         init::LateResources {
             spi: (dp.SPI1, dp.SPI2, dp.SPI4, dp.SPI5),
             tim: dp.TIM1,
@@ -594,7 +523,7 @@ const APP: () = {
                     }
             }
         }
-
+        /*
         let sr = spi5.sr.read();
         if sr.eot().bit_is_set() {
             spi5.ifcr.write(|w| w.eotc().set_bit());
@@ -612,6 +541,7 @@ const APP: () = {
         unsafe { ptr::write_volatile(txdr, d) };
         #[cfg(feature = "bkpt")]
         cortex_m::asm::bkpt();
+        */
     }
 
     /*