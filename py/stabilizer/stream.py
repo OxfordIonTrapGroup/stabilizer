5faf04702c3b922256415145d453ec4f0f416ee3
mollysmith23 <118980653+mollysmith23@users.noreply.github.com>
2026-02-19 17:03:52 +0000

    Add more checks

--- py/stabilizer/stream.py
+++ py/stabilizer/stream.py
@@ -212,10 +212,13 @@ class StabilizerStream(asyncio.DatagramProtocol):
             sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, group + iface)
             sock.bind(('', port))
         else:
-            sock.bind(("0.0.0.0", port))
+            sock.bind((addr, port))
             print("Socket bound to:", sock.getsockname())
 
         transport, protocol = await loop.create_datagram_endpoint(lambda: cls(maxsize, _parsers), sock=sock)
+        
+        print("Protocol")
+        print(protocol)
         return transport, protocol
 
     def __init__(self, maxsize, parsers):
