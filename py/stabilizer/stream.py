fe945308cfcb48c8ff32aadf1fe6857a438fbb57
mollysmith23 <118980653+mollysmith23@users.noreply.github.com>
2026-02-19 17:51:53 +0000

    undid

--- py/stabilizer/stream.py
+++ py/stabilizer/stream.py
@@ -193,7 +193,6 @@ class StabilizerStream(asyncio.DatagramProtocol):
         _parsers = {parser.format_id: parser for parser in parsers}
 
         loop = asyncio.get_running_loop()
-        print("Transport loop id:", id(loop))
         print("open() loop:", asyncio.get_running_loop())
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -208,20 +207,15 @@ class StabilizerStream(asyncio.DatagramProtocol):
         # We need to specify which interface to receive broadcasts from, or Windows may choose the
         # wrong one. Thus, use the broker address to figure out our local address for the interface
         # of interest.
-        # if ipaddress.ip_address(addr).is_multicast:
-        #     print('Subscribing to multicast')
-        #     group = socket.inet_aton(addr)
-        #     iface = socket.inet_aton('.'.join([str(x) for x in get_local_ip(broker)]))
-        #     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, group + iface)
-        #     sock.bind(('', port))
-        # else:
-        #     sock.bind((addr, port))
-        #     print("Socket bound to:", sock.getsockname())
-        transport, protocol = await loop.create_datagram_endpoint(
-            lambda: cls(maxsize, _parsers),
-            local_addr=("0.0.0.0", port),
-            family=socket.AF_INET
-        )
+        if ipaddress.ip_address(addr).is_multicast:
+            print('Subscribing to multicast')
+            group = socket.inet_aton(addr)
+            iface = socket.inet_aton('.'.join([str(x) for x in get_local_ip(broker)]))
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, group + iface)
+            sock.bind(('', port))
+        else:
+            sock.bind((addr, port))
+            print("Socket bound to:", sock.getsockname())
         
         transport, protocol = await loop.create_datagram_endpoint(lambda: cls(maxsize, _parsers), sock=sock)
         
